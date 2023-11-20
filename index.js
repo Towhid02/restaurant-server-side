bb800e4459b015900931fa19703524e4daf8256e
Md Towhidul Islam <towhidul11fahim@gmail.com>
2023-11-20 11:48:03 +0600

    added the fifth blog

--- index.js
+++ index.js
@@ -81,6 +81,13 @@ async function run() {
           res.send(result);
         })
 
+        app.delete('/menu/:id', async(req, res)=> {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id)}
+          const result= await menuCollection.deleteOne(query);
+          res.send(result)
+        })
+
         app.put('/menu/:id', async(req, res)=>{
           const id = req.params.id;
           const filter = { _id: new ObjectId(id)};
