99d559f43eca3e1b74a90584247afa4fbd4ba5a9
Md Towhidul Islam <towhidul11fahim@gmail.com>
2023-11-21 10:30:51 +0600

    added the forth b;pg

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-      origin: ['http://localhost:5173'],
+      origin: [
+        // 'http://localhost:5173'
+        'https://grill-bar-b3e50.web.app',
+        'https://grill-bar-b3e50.firebaseapp.com/'
+      ],
       credentials: true,
   }),
 )
 app.use(cors());
 app.use(express.json()); 
 
-// custom middleware
+
 // console.log(DB_USER)
 // console.log(DB_PASS)
 
